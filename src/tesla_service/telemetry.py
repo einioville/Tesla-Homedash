5c3a932e4403780ff30e29d5ef1f39bbec6b30ad
Hanzanka <ville.einio@gmail.com>
2026-01-24 16:14:55 +0200

    Add: Restart stream in case of connection loss

--- src/tesla_service/telemetry.py
+++ src/tesla_service/telemetry.py
@@ -2,7 +2,6 @@ import asyncio
 from aiohttp import ClientSession
 from teslemetry_stream import TeslemetryStream
 from .vehicle import Vehicle
-from .server import TeslaDataServer
 
 class TelemetryHandler:
     def __init__(self, access_token: str, server: str, vehicle: Vehicle) -> None:
@@ -14,22 +13,27 @@ class TelemetryHandler:
         self.__close_event = asyncio.Event()
 
     async def start(self) -> None:
-        async with ClientSession() as session:
-            self.__stream = TeslemetryStream(
-                access_token=self.__access_token,
-                vin=await self.__vehicle.get_vin(),
-                server=self.__server,
-                session=session,
-                parse_timestamp=True,
-            )
+        while not self.__close_event.is_set():
+            try:
+                async with ClientSession() as session:
+                    print("Stream started")
+                    self.__stream = TeslemetryStream(
+                        access_token=self.__access_token,
+                        vin=await self.__vehicle.get_vin(),
+                        server=self.__server,
+                        session=session,
+                        parse_timestamp=True,
+                    )
 
-            await self.__stream.connect()
+                    await self.__stream.connect()
 
-            self.__close_stream = self.__stream.async_add_listener(
-                self.__vehicle.on_telemetry_event
-            )
+                    self.__close_stream = self.__stream.async_add_listener(
+                        self.__vehicle.on_telemetry_event
+                    )
 
-            await self.__close_event.wait()
+                    await self.__close_event.wait()
+            except Exception:
+                await asyncio.sleep(5)
 
     def close(self) -> None:
         if self.__close_stream is not None:
